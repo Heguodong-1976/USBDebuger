857550e815668d5caa17ffe9f7707e5c8673e496
guodong he <32354456@qq.com>
2022-06-11 22:59:40 +0800

    aa

--- Source/main.cpp
+++ Source/main.cpp
@@ -38,27 +38,27 @@ static Error* print_devs(libusb_device **devs,ostream& s)
 		struct libusb_device_descriptor desc;
 		auto ret = libusb_get_device_descriptor(dev, &desc);
 		if (ret < 0) {
-			cerr<<"Failed libusb_get_device_descriptor(...)"<<endl;
-			//return new Error{"Failed libusb_get_device_descriptor(...)",ret,__FILE__,__LINE__};
+			s<<"Failed libusb_get_device_descriptor(...)"<<endl;
+			return new Error{"Failed libusb_get_device_descriptor(...)",ret,__FILE__,__LINE__};
 		}
 		
-		cout<<dec<<i<<"  Vendor:"<<hex<<desc.idVendor<<"\tProduct:"<<desc.idProduct;
+		s<<dec<<i<<"  Vendor:"<<hex<<desc.idVendor<<"\tProduct:"<<desc.idProduct;
 		auto bus=libusb_get_bus_number(dev);
 		auto address=libusb_get_device_address(dev);
-		cout<<"\tBus:"<<(int)bus<<"\tAddress:"<<(int)address;
+		s<<"\tBus:"<<(int)bus<<"\tAddress:"<<(int)address;
 		
 		uint8_t path[8]; 
 		ret = libusb_get_port_numbers(dev, path, sizeof(path));
-		cout<<"\tPath:";
+		s<<"\tPath:";
 		for (int j = 0; j < ret; j++)
-			cout<<"."<<(int)path[j];
-		cout<<"\t";		
+			s<<"."<<(int)path[j];
+		s<<"\t";		
 		
 		libusb_device_handle *dev_handle=NULL;
 		ret=libusb_open(dev,&dev_handle);
 		if(ret==libusb_error::LIBUSB_ERROR_NOT_SUPPORTED)
 		{
-			cout<<"\tFailed libusb_open(...)==LIBUSB_ERROR_NOT_SUPPORTED"<<endl;
+			s<<"\tFailed libusb_open(...)==LIBUSB_ERROR_NOT_SUPPORTED"<<endl;
 			continue;
 		}
 		
@@ -69,9 +69,9 @@ static Error* print_devs(libusb_device **devs,ostream& s)
 		ret=libusb_get_string_descriptor_ascii(dev_handle,desc.iSerialNumber,serialNumber,255);
 		ret=libusb_get_string_descriptor_ascii(dev_handle,desc.iManufacturer,manufacturer,255);
 		libusb_close(dev_handle);
-		cout<<"\tSuccess:"<<serialNumber<<":"<<product<<":"<<manufacturer;
+		s<<"\tSuccess:"<<serialNumber<<":"<<product<<":"<<manufacturer;
 
-		cout<<endl;
+		s<<endl;
 	}
 	return NULL;	
 }
